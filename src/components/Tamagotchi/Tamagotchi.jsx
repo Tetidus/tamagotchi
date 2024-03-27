be432c428964133f66288795db6ce53d40b940f5
Tetidus <mattia.panzeri9898@gmail.com>
2024-03-27 21:33:33 +0100

    fix diminuzione happiness con hungry = 100

--- src/components/Tamagotchi/Tamagotchi.jsx
+++ src/components/Tamagotchi/Tamagotchi.jsx
@@ -1,35 +1,34 @@
 import React, { useState, useEffect, useContext } from 'react';
 import './Tamagotchi.css';
 import hero_idle from '../../assets/hero_idle.gif';
-import poopImg from '../../assets/poop.png'; // Assicurati di avere questo file in assets
+import poopImg from '../../assets/poop.png'; 
 import InteractionPanel from '../InteractionPanel/InteractionPanel';
 import { getDatabase, ref, set, onValue } from 'firebase/database';
-import AuthContext from '../../authContext'; // Assicurati che il percorso sia corretto
+import AuthContext from '../../authContext'; 
 import { doSignOut } from '../../auth';
 import { useNavigate } from 'react-router-dom';
 
 const Tamagotchi = () => {
     const [position, setPosition] = useState(0);
     const [poops, setPoops] = useState([]);
-    const [happiness, setHappiness] = useState(30);
+    const [happiness, setHappiness] = useState(0);
     const [hunger, setHunger] = useState(0);
     const [energy, setEnergy] = useState(50);
-    const { currentUser } = useContext(AuthContext); // Usa l'AuthContext per ottenere l'utente attuale
-    const [isDataLoaded, setIsDataLoaded] = useState(false); // Sposta questa riga qui
+    const { currentUser } = useContext(AuthContext); 
+    const [isDataLoaded, setIsDataLoaded] = useState(false); 
     const navigate = useNavigate()
 
     useEffect(() => {
         const db = getDatabase();
         const statusRef = ref(db, 'users/' + currentUser.uid + '/status');
 
-        // Restituisce una funzione che può essere usata per rimuovere il listener.
         const unsubscribe = onValue(statusRef, (snapshot) => {
             const data = snapshot.val();
             if (data) {
-                setHappiness(data.happiness || 30); // Usa un valore di default se non definito
+                setHappiness(data.happiness || 0); 
                 setHunger(data.hunger || 0);
                 setEnergy(data.energy || 50);
-                setIsDataLoaded(true); // Aggiungi questa riga
+                setIsDataLoaded(true); 
             }
         });
 
@@ -58,31 +57,23 @@ const Tamagotchi = () => {
         const hungerInterval = setInterval(() => {
             setHunger(prevHunger => {
                 const newHunger = prevHunger + 10;
-                return newHunger <= 100 ? newHunger : 100; // Limita il valore massimo a 100
+                return newHunger <= 100 ? newHunger : 100; 
             });
-        }, 5000); // Ogni 5 secondi
-
-        const happinessInterval = setInterval(() => {
-            if (hunger === 100 && happiness > 0) {
-                setHappiness(prevHappiness => Math.max(prevHappiness - 10, 0)); // Imposta happiness a 0 se il nuovo valore è inferiore a 0
-            }
-        }, 5000); // Ogni 5 secondi
+        }, 5000);
 
         return () => {
             clearInterval(moveInterval);
             clearInterval(poopInterval);
             clearInterval(hungerInterval);
-            clearInterval(happinessInterval);
+            // clearInterval(happinessInterval);
             unsubscribe();
         };
     }, [currentUser.uid]);
 
     useEffect(() => {
-        if (!isDataLoaded || !currentUser || !currentUser.uid) return; // Assicurati che l'uid dell'utente esista
+        if (!isDataLoaded || !currentUser || !currentUser.uid) return; 
 
         const db = getDatabase();
-        // Qui, costruisci il percorso dove vuoi salvare i dati dentro il tuo database Firebase
-        // Nota: si suppone che `currentUser` sia già definito; altrimenti, gestisci questo caso.
         set(ref(db, 'users/' + currentUser.uid + '/status'), {
             happiness,
             hunger,
@@ -90,7 +81,19 @@ const Tamagotchi = () => {
         }).catch(error => {
             console.error("Firebase set error:", error);
         });
-    }, [happiness, hunger, energy, currentUser, isDataLoaded]); // Dipendenze useEffect
+    }, [happiness, hunger, energy, currentUser, isDataLoaded]); 
+
+    useEffect(() => {
+        const happinessInterval = setInterval(() => {
+            if (hunger === 100) {
+                setHappiness(prevHappiness => Math.max(prevHappiness - 10, 0));
+            }
+        }, 5000);
+
+        return () => {
+            clearInterval(happinessInterval);
+        };
+    }, [hunger]);
 
     const removePoop = id => {
         setPoops(prevPoops => prevPoops.filter(poop => poop.id !== id));
@@ -112,7 +115,7 @@ const Tamagotchi = () => {
     };
 
     if (!isDataLoaded) {
-        return <div>Caricamento...</div>; // Oppure qualsiasi altro markup/componente per il caricamento
+        return <div>Caricamento...</div>; 
     }
 
     return (
@@ -136,7 +139,7 @@ const Tamagotchi = () => {
                 onSleep={putTamagotchiToSleep}
             />
 
-<br /><br />
+            <br /><br />
             <button onClick={() => { doSignOut().then(() => { navigate('/login') }) }}>Sign Out</button>
 
         </div>
